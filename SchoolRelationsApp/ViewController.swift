b1eab7574be97ab20a2b9a6d267fe094776adca0
saba khitaridze <sabaxitara@icloud.com>
2021-11-05 19:29:40 +0400

    Initial Commit

---
+++ SchoolRelationsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SchoolRelationsApp
+//
+//  Created by Saba Khitaridze on 05.11.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
